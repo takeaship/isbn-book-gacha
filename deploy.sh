e8ab323e643d2096301ba32fd3d334a9424a864d
takeaship <spiral.spirit.spider@gmail.com>
2022-12-04 17:10:14 +0900

    [add] デプロイ用スクリプトの追加

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:takeaship/isbn-book-gacha.git main:gh-pages
+
+cd -
